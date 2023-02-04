d5d63b4bc335f63fb488f06500080ad03504e1a1
Yo <toaninja1983@gmail.com>
2023-02-04 14:54:20 +0700

    Begin

---
+++ teleapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+		user = models.OneToOneField(User,on_delete=models.CASCADE)#การใช้ .OneToOneField คือ การอ้างอิงถึง field ของ tadateble อื่น ในที่นี้ อ้างอิงถึง data table User หากมีการลบให้ลบตรงนี้ด้วย
+		photo = models.ImageField(upload_to = 'photo_profile',null=True,blank=True,default='default.png')
+		usertype = models.CharField(max_length = 100,default='member')
+		def __str__ (self):
+			return str(self.user)
---
+++ teleapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from teleapp.models import * 
+from django.contrib import messages
+
+
+# Create your views here.
+def index(request):
+    persons = Profile.objects.all()
+
+    return render(request,'index.html',{"persons":persons})
+
+def dashboard(request):
+    persons = Profile.objects.all()
+
+    return render(request,'dashboard.html',{"persons":persons})
